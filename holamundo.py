edd7a89bb5c6258a2b81619eb482957b0a9b5299
FcoJimenez <francisco.jimenez.alcantara@gmail.com>
2024-09-04 15:47:31 -0400

    Hola Mundo by Python

---
+++ holamundo.py
@@ -0,0 +1,9 @@
+# Hola Mundo! [Python]
+# ---------------------------
+# Autor: Francisco Jiménez A.
+
+# Descripcion: Programa creado para ensayar el correcto funcionamiento
+# del entorno de instalación Python
+#python -m venv venv
+#venv\scripts\activate
+print("Hola Mundo! ;P")
